719925c6a2665fc5d9c5c4366d02b264f6bfc863
Justin Crown <justin.crown@spinifexgroup.com>
2017-11-11 13:26:38 -0800

    dummy django-q task

--- kiwi_pycon_2017_demo/views.py
+++ kiwi_pycon_2017_demo/views.py
@@ -1,3 +1,6 @@
+from math import copysign
+
+from django_q.tasks import async
 from rest_framework import viewsets
 
 from .models import Widget
@@ -7,3 +10,8 @@ from .serializers import WidgetSerializer
 class WidgetViewSet(viewsets.ModelViewSet):
     serializer_class = WidgetSerializer
     queryset = Widget.objects.all()
+
+    def create(self, *args, **kwargs):
+        task_id = async(copysign, 2, -2)
+        print('Created super important task with id {0}'.format(task_id))
+        return super().create(*args, **kwargs)
