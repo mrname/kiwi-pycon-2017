1ded17ab5833e247677ffe88387105d227d3d845
Justin Crown <justin.crown@spinifexgroup.com>
2017-12-01 10:32:45 -0800

    stupid hack to wait for database startup

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# NOTE - this sleep is a stupid hack to keep our demo simple. The point is
+# that we need the database server to be up and running before we do our
+# database migrations. There are better ways to do this!
+# https://docs.docker.com/compose/startup-order/
+sleep 5
+
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
